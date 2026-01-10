bf83636e2f1be1edb171f87da833db1646812249
fk-du <fkadugoitomw@gmail.com>
2026-01-10 10:39:02 +0300

    curency-conversion-service

---
+++ curency-conversion-service/curency-conversion-service/src/main/java/com/fkadu/microservices/curency_conversion_service/CurencyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fkadu.microservices.curency_conversion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurencyConversionServiceApplication.class, args);
+	}
+
+}
---
+++ curency-conversion-service/curency-conversion-service/src/test/java/com/fkadu/microservices/curency_conversion_service/CurencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fkadu.microservices.curency_conversion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
